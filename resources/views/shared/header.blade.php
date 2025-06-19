f9e27cd590a09b3258d2988411520e0951952a00
Neycer-Nay <neyser.nay@gmail.com>
2025-06-19 15:45:53 -0400

    menu ususarios reconoce rol y nombre

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -5,22 +5,29 @@
             <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
             <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"></a></li>
         </ul>
-        
+
     </form>
     <ul class="navbar-nav navbar-right">
-        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
-                class="nav-link nav-link-lg "></a>
-            
+        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg "></a>
+
         </li>
-        
+
         <li class="dropdown"><a href="#" data-toggle="dropdown"
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                 <img alt="image" src="{{ asset('dist/assets/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">{{Auth::user()->nombre}}</div>
             </a>
-            
+
             <div class="dropdown-menu dropdown-menu-right">
                 
+                
+                <a href="features-profile.html" class="dropdown-item has-icon">
+                    <i class="far fa-user"></i> {{ Auth::user()->nombre }}
+                </a>
+                
+                <a href="features-settings.html" class="dropdown-item has-icon">
+                    <i class="fas fa-cog"></i> {{ Auth::user()->rol }}
+                </a>
                 <div class="dropdown-divider"></div>
                 <a href="{{ asset('logout') }}" class="dropdown-item has-icon text-danger">
                     <i class="fas fa-sign-out-alt"></i> Salir
