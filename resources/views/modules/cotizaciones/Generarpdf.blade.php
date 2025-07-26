e13d9faa7aa202f945a0493bcd590d6c741c0612
Neycer-Nay <neyser.nay@gmail.com>
2025-07-26 17:13:58 -0400

    al generar pdf tipo de motor se cambio el formato de las letras con srt::title

--- resources/views/modules/cotizaciones/Generarpdf.blade.php
+++ resources/views/modules/cotizaciones/Generarpdf.blade.php
@@ -174,7 +174,7 @@
                 <th colspan="5" style="background-color: #000d53; color: #fff;">NOMBRE DEL EQUIPO:{{ $equipo->nombre }}</th>
             </tr>
             <tr>
-                <td><strong>Tipo:</strong> {{ $equipo->tipo }}</td>
+                <td><strong>Tipo:</strong> {{  Str::title(str_replace('_', ' ', $equipo->tipo))}}</td>
                 
                 <td><strong>Marca:</strong> {{ $equipo->marca }}</td>
                 <td><strong>Modelo:</strong> {{ $equipo->modelo }}</td>
