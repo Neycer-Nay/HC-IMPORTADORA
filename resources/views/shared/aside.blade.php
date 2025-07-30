f1c294c338d9e5c315e974ae1f7644f3ccbf2311
Neycer-Nay <neyser.nay@gmail.com>
2025-07-30 09:28:37 -0400

    Se agrego en sub menu de contabilidad con opciones de recibos, egresos, ingresos y reportes

--- resources/views/shared/aside.blade.php
+++ resources/views/shared/aside.blade.php
@@ -38,7 +38,22 @@
             <li class="dropdown">
                 <a href="{{ route('clientes.index') }}" class="nav-link "><i class="fas fa-users"></i>
                     <span>Clientes</span></a>
-
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown">
+                    <i class="fas fa-calculator"></i> <span>Contabilidad</span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#"><i
+                                class="fas fa-file-invoice-dollar"></i> Recibos</a></li>
+                    <li><a class="nav-link" href="#"><i
+                                class="fas fa-arrow-down"></i> Ingresos</a></li>
+                    <li><a class="nav-link" href="#"><i class="fas fa-arrow-up"></i>
+                            Egresos</a></li>
+                    <li><a class="nav-link" href="#"><i
+                                class="fas fa-chart-line"></i> Reportes</a></li>
+                </ul>
+            </li>
         </ul>
 
 
